b03ec2a5a601a13e39a579fe379f52936e02a368
gnozadi <56971838+gnozadi@users.noreply.github.com>
2023-11-16 14:13:14 +0330

    Create App.js

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    
+    </div>
+  );
+}
+
+export default App;
