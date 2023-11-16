11758f3c3863b386f805a8fcb704721d61f562b0
gnozadi <56971838+gnozadi@users.noreply.github.com>
2023-11-16 14:18:13 +0330

    Create Dashboard.js

---
+++ front/src/Components/Dashboard.js
@@ -0,0 +1,14 @@
+import Navbar from "../Bars/Navnbar"
+import BlackSidebar from "../Bars/Sidebar";
+
+const Dashboard = () => {
+return(
+    <>
+    
+    <Navbar/>
+    <BlackSidebar/>
+    </>
+);
+}
+
+export default Dashboard
